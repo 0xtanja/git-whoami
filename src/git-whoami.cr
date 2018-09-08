d3a22746c58b2817cd655b7482dd18f68f225070
Tanja Alva <tanja.alva@gmail.com>
2018-09-08 13:56:02 +0800

    Add handler for when user can't be resolved

--- src/git-whoami.cr
+++ src/git-whoami.cr
@@ -4,10 +4,13 @@ require "./git-whoami/*"
 module Git::Whoami
   stdout = IO::Memory.new
   stderr = IO::Memory.new
-  status = Process.run("git config --local --get-regexp use", output: stdout, error: stderr, shell: true)
+  status = Process.run "git config --local --get-regexp user", output: stdout, error: stderr, shell: true
   if status.success?
     STDOUT.puts stdout.to_s
   else
+    if stderr.empty?
+      stderr.write "No user could be resolved.".to_slice
+    end
     STDERR.puts stderr.to_s
   end
 end
